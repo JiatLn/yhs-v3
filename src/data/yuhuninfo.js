9b1c94033adb26789d6b62cdc62a007fef7c8580
林佳涛 <lne128gene980@163.com>
2021-07-01 17:30:45 +0800

    添加临时的二件套图标

--- src/data/yuhuninfo.js
+++ src/data/yuhuninfo.js
@@ -244,38 +244,39 @@ const yuhunOptions = Object.entries(yuhunDict).map(([key, value]) => ({
 
 const twoSuitOptions = [
   {
-    name: '暴击',
-    id: 300010,
-    type: '暴击',
-  },
-  {
     name: '攻击加成',
-    id: 300010,
+    id: 6,
     type: '攻击加成',
   },
   {
+    name: '暴击',
+    id: 4,
+    type: '暴击',
+  },
+  {
     name: '首领御魂',
-    id: 300010,
+    id: 2,
     type: '首领御魂',
   },
   {
+    name: '生命加成',
+    id: 5,
+    type: '生命加成',
+  },
+  {
     name: '效果命中',
-    id: 300010,
+    id: 8,
     type: '效果命中',
   },
   {
     name: '效果抵抗',
-    id: 300010,
+    id: 7,
     type: '效果抵抗',
   },
-  {
-    name: '生命加成',
-    id: 300010,
-    type: '生命加成',
-  },
+
   {
     name: '防御加成',
-    id: 300010,
+    id: 1,
     type: '防御加成',
   },
 ];
